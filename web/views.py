1dbe20b734c22c19dd53267a5b84da842743f096
Sang Nguyễn <nguyennhubaosang@gmail.com>
2021-09-20 15:46:14 +0700

    ok

---
+++ web/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+from .forms import CommentForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'profile.html')
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
